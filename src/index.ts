0bff03d60580d564b75b41991a3209bfdebacc1f
Alexandre_oliveira <alexandredev.frontend@gmail.com>
2024-08-08 21:19:57 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 import express, {NextFunction, Request, Response} from "express";
 import routes from "./router/router";
 import cors from "cors";
+import "express-async-errors";
+
 
 export const service = express();
-export function App() {
-  service.use(cors({origin: "*"}));
-  service.use((req: Request, res: Response, next: NextFunction) => {
-    express.json()(req, res, next);
-  });
-  service.use(routes);
-  service.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-      return res.status(400).json({
-        error: err.message,
-      });
-    }
-
-    return res.status(500).json({
-      status: "error",
-      message: "Internal server error.",
+service.use(cors({origin: "*"}));
+service.use((req: Request, res: Response, next: NextFunction) => {
+  express.json()(req, res, next);
+});
+service.use(routes);
+service.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
     });
-  });
+  }
 
-  service.get("/", (req: Request, res: Response) => {
-    return res.json({message: "ok"});
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error.",
   });
+});
 
-  service.listen(process.env.PORT, () => console.log("api online"));
-}
-App();
+service.get("/", (req: Request, res: Response) => {
+  return res.json({message: "ok"});
+});
+
+service.listen(5000, () => console.log("api online"));
